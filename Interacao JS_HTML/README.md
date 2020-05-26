726413f71f3eaa6e40821108b4a9434b6e394208
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-05-26 12:30:28 -0300

    Organização de pastas

--- Interacao JS_HTML/README.md
+++
@@ -1,7 +0,0 @@
-# Interacao JS_HTML
-Usando funções como getElementById, e inserindo HTML através do Java Script.
-
-* **Imput**  
-Pegando valor do Imput com o JS
-* **CreateElement**  
-Usando e manipulando elementos DOM
--- Request API/axios/axios.js
+++
@@ -1,8 +0,0 @@
-axios.get('https://api.github.com/users/matheusbecatini')
-    .then(response => {
-        console.log('Using .then .catch')
-        console.log(response)
-    })
-    .catch(error => {
-        console.warn('Erro no .then .catch ' + error)
-    })
