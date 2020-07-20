bd11aea09db7577a0f939fe40abb07548e9ac58c
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-07-20 18:08:23 -0300

    Adicionado projeto de exemplo Dotnet

---
+++ C#/Examples/Examples/Functions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples
+{
+    class Functions
+    {
+        public int soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ C#/Examples/Examples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Functions();
+
+            Console.WriteLine(f.soma(2, 2));
+        }
+    }
+}
